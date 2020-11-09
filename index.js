76d34c4791fed60b7b7b05e636ea1265a87d11f0
sky192 <samahehloghmani@gmail.com>
2020-11-08 23:20:26 -0500

    did some styles

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const showdata = require('./showdata')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+
+app.get('/', (request, response) => {
+  return response.render('index', { showdata })
+})
+
+app.get('/season/:id', (request, response) => {
+  const season = showdata.seasons.find((season) => season.number === parseInt(request.params.id))
+
+  return response.render('season', { season, title: showdata.title })
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+app.listen(1444, () => {
+  // eslint-disable-next-line no-console
+  console.log('beep beep.... not working....')
+})
